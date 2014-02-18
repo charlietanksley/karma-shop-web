30f119450a1f639fd1cfe3d61d355a4e2aeaf8d7
Charlie Tanksley <charlie.tanksley@gmail.com>
2014-02-18 07:06:27 -0500

    add model and collection for potential customers
    I'm going to start trying to do things the backbone way. :smile_cat:

---
+++ app/js/models/potential-customers.js
@@ -0,0 +1,16 @@
+var PotentialCustomer = require('./potential-customer')
+, fn = require('../fn')
+
+module.exports = Backbone.Collection.extend({
+  model: PotentialCustomer
+, url: 'api/potential_customers.json'
+
+, parse: function(response) {
+    var convertToPotentialCustomers = function(entry) {
+      return { name: entry }
+    }
+
+    return fn.map(response.potentialCustomers,
+                  convertToPotentialCustomers)
+  }
+})
