4438a6c53d41d3f90966eadec045a1a2673b4c4f
Charlie Tanksley <charlie.tanksley@gmail.com>
2014-02-19 10:32:32 -0500

    add view and template for treasures
    This name clash is undesirable.  I need to figure out how to work with
    lineman to make it not happen.

---
+++ app/js/views/treasure-view.js
@@ -0,0 +1,29 @@
+var Backbone = require('exoskeleton')
+
+module.exports = Backbone.View.extend({
+  el: '.content'
+
+, initialize: function() {
+    this.collection.on("sync", this.setCustomer.bind(this))
+    this.collection.on("sync", this.render.bind(this))
+  }
+
+, setCustomer: function() {
+    this.customer = this.collection.customer
+  }
+
+, template: '<div class="treasure-listing"></div>'
+
+, render: function() {
+    var TreasureView = require('../../views/treasure-view.jsx')
+      , React = require('react')
+      , data = { customer: this.customer,
+                 treasures: this.collection }
+
+    this.el.innerHTML = this.template
+    var component = document.getElementsByClassName('treasure-listing')[0]
+
+    React.renderComponent(new TreasureView(data), component)
+    return this
+  }
+})
