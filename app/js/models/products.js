988823e2cd3b9545e4cf53be9707c5839855fa83
Charlie Tanksley <charlie.tanksley@gmail.com>
2014-02-20 13:37:07 -0500

    add product model and collection

---
+++ app/js/models/products.js
@@ -0,0 +1,28 @@
+var Product = require('./product')
+  , Customer = require('./customer')
+  , fn = require('../fn')
+
+module.exports = Backbone.Collection.extend({
+  model: Product
+, url: '/api/products.json'
+
+, initialize: function(options) {
+    this.customerName = options.customerName
+  }
+
+, fetch: function(options) {
+    options = options === undefined ? {} : options
+    options.data = options.data === undefined ? {} : options.data
+    options.data = fn.extend(options.data, {customerName: this.customerName})
+
+    return Backbone.Collection.prototype.fetch.call(this, options)
+  }
+
+, parse: function(response) {
+    var customer = response.store.customer,
+        products = response.store.products
+
+    this.customer = new Customer(customer)
+    return products
+  }
+})
