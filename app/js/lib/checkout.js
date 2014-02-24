9daa7ede2a0a83a4013ff4166f29e6311568f314
Charlie Tanksley <charlie.tanksley@gmail.com>
2014-02-24 07:22:40 -0500

    people cannot buy more than they can afford

--- app/js/lib/checkout.js
+++ app/js/lib/checkout.js
@@ -4,29 +4,44 @@ var fn = require('../fn')
 module.exports = function(customer) {
   this.customer = customer
 
-  this.process =  function(product) {
+  this.process =  function(product, insufficientFundsCallback) {
     this.product = product
 
-    this.makePurchase()
-    this.decrementKarma()
-    this.incrementUnitsOwned()
+    if (this.checkPurchaseAllowed(insufficientFundsCallback)) {
+      this.makePurchase()
+      this.decrementKarma()
+      this.incrementUnitsOwned()
+    }
   }
 
   this.decrementKarma = function() {
-    var customer = this.customer
-      , product = this.product
-    var availableKarma = customer.get('availableKarma')
-      , price = product.get('price')
-
-    customer.set('availableKarma', availableKarma - price)
+    this.customer.set('availableKarma', this.karmaDecremented())
   }
 
-  this.incrementUnitsOwned = function(product) {
+  this.karmaDecremented = function() {
+    return this.customer.get('availableKarma') - this.product.get('price')
+  }
+
+  this.incrementUnitsOwned = function() {
     var unitsOwned = this.product.get('unitsOwned') + 1
     this.product.set('unitsOwned', unitsOwned)
   }
 
-  this.makePurchase = function(customer, product) {
+  this.checkPurchaseAllowed = function(callback) {
+    var popupAlert = function() {
+      alert('You cannot afford this item')
+    }
+    var notify = callback == undefined ? popupAlert : callback
+
+    if (this.karmaDecremented() >= 0) {
+      return true
+    } else {
+      notify()
+      return false
+    }
+  }
+
+  this.makePurchase = function() {
     var attrs = {
       customerName: this.customer.get('name')
     , productId: this.product.get('id')
