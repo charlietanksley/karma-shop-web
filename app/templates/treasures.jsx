f805262a3fc00995f62d6d37f64162365c90cfd5
Charlie Tanksley <charlie.tanksley@gmail.com>
2014-02-25 12:36:38 -0500

    fix bad math caused by 0 indexed arrays
    Also spacing got weird b/c jsx mode.  Emacs!!!!!!

--- app/templates/treasures.jsx
+++ app/templates/treasures.jsx
@@ -2,31 +2,32 @@
 var React = require('react')
 
 module.exports = React.createClass({
-    render: function() {
-        var classForListItem = function(index) {
-            return index % 4 == 0 ? "treasure last" : "treasure"
-        }
-
-        var listNodes = this.props.treasures.map(function (treasure, index) {
-            return (
-                <li className={ classForListItem(index) }>
-                  <img className="treasure-picture" src={ treasure.get('src') } alt={ treasure.get('name') } />
-                  <small className="attribution">
-                    <a href={ treasure.get('attributionUrl') }>
-                      { treasure.get('attributionText') }
-                    </a>
-                  </small>
-                </li>
-            )
-        })
-
-        return(
-            <section className="treasures">
-              <h2 className="treasure-owner">{ this.props.customer.get('name') }</h2>
-              <ul className="plain">
-                { listNodes }
-              </ul>
-            </section>
-        )
+  render: function() {
+    var classForListItem = function(index) {
+      var i = index + 1
+      return i % 4 == 0 ? "treasure last" : "treasure"
     }
+
+    var listNodes = this.props.treasures.map(function (treasure, index) {
+      return (
+        <li className={ classForListItem(index) }>
+        <img className="treasure-picture" src={ treasure.get('src') } alt={ treasure.get('name') } />
+        <small className="attribution">
+        <a href={ treasure.get('attributionUrl') }>
+        { treasure.get('attributionText') }
+        </a>
+        </small>
+        </li>
+      )
+    })
+
+    return(
+      <section className="treasures">
+      <h2 className="treasure-owner">{ this.props.customer.get('name') }</h2>
+      <ul className="plain">
+      { listNodes }
+      </ul>
+      </section>
+    )
+  }
 })
