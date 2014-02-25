deaa05f3e607449f82fcc9d845147956e47e7bb3
Charlie Tanksley <charlie.tanksley@gmail.com>
2014-02-25 12:37:16 -0500

    make sidebar link to user treasure pages

--- app/templates/potential-customers.jsx
+++ app/templates/potential-customers.jsx
@@ -2,15 +2,18 @@
 var React = require('react')
 
 module.exports = React.createClass({
-    render: function() {
-        var listNodes = this.props.potentialCustomers.map(function (potentialCustomer) {
-            return <li>{ potentialCustomer.get('name') }</li>
-        })
+  render: function() {
+    var listNodes = this.props.potentialCustomers.map(function (potentialCustomer) {
+      var name = potentialCustomer.get('name')
+      , link = "/treasures/" + name
 
-        return(
-            <ul className="plain">
-            { listNodes }
-            </ul>
-        )
-    }
+      return <li><a href={ link } >{ name }</a></li>
+    })
+
+    return(
+      <ul className="plain">
+      { listNodes }
+      </ul>
+    )
+  }
 })
