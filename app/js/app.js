5734f733af9606f056da7e5ba6111a5488d41ae7
Charlie Tanksley <charlie.tanksley@gmail.com>
2014-03-06 06:59:25 -0500

    actually put navigation on page

--- app/js/app.js
+++ app/js/app.js
@@ -4,4 +4,11 @@
 
   new Router()
   Backbone.history.start({pushState: true})
+
+  var NavigationView = require('./views/navigation-view')
+    , PotentialCustomers = require('./models/potential-customers')
+    , potentialCustomers = new PotentialCustomers()
+
+  potentialCustomers.fetch()
+  new NavigationView({collection: potentialCustomers})
 })()
