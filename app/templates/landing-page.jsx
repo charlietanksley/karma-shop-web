53188853006f718187a46efb1eb49510b561f3e8
Charlie Tanksley <charlie.tanksley@gmail.com>
2014-03-06 09:32:30 -0500

    whitespace

--- app/templates/landing-page.jsx
+++ app/templates/landing-page.jsx
@@ -2,14 +2,14 @@
 var React = require('react')
 
 module.exports = React.createClass({
-    render: function() {
-        return(
-            <p>
-              You have all that karma sitting in the (chatroom) bank.
-              Are you really enjoying it?  Are you really <i>living?</i>
-              Don't wait until retirement to enjoy your karma.  Treat
-              yourself to something nice today.  You've earned it.
-            </p>
-        )
-    }
+  render: function() {
+    return(
+      <p>
+        You have all that karma sitting in the (chatroom) bank.
+        Are you really enjoying it?  Are you really <i>living?</i>
+        Don't wait until retirement to enjoy your karma.  Treat
+         yourself to something nice today.  You've earned it.
+      </p>
+    )
+  }
 })
